6e2f466305253a8e18b40e7b203f69bae2322067
antonio8torres1 <antonio8torres1@gmail.com>
2025-04-28 17:48:57 -0600

    Se agrega una posible funcion para encontrar la variable de salida

--- table.py
+++ table.py
@@ -4,27 +4,20 @@ from numpy import array
 from element_row import ElementRow
 
 class TableSimplex:
-    def __init__(self , num_ecu, variables, pos):
+    def __init__(self , num_ecu, variables):
         self.table = np.zeros(num_ecu, dtype=ElementRow)
-        self.positions = pos
         self.num_variables = variables
         self.num_equations = num_ecu
         self.basic_variables = np.zeros(num_ecu , dtype="U10")
+        self.variable = np.zeros(variables + num_ecu + 1, dtype= "U10")
 
     def __str__(self):
         leap = (self.num_equations + self.num_variables + 1) * 10
         table = "-" * leap
-        table += "\n|"
-        for i in range(self.num_variables + 1):
-            if i == 0:
-                table += f"   VB" + "   | "
-            else:
-                table += f"   X{i}" + "   | "
-
-        for i in range(len(self.table) - 1):
-            table += f"   H{i + 1}" + "   | "
-
-        table += "  LD    |" + "\n"
+        table += "\n|   "
+        for i in range(len(self.variable)):
+            table +=   self.variable[i] + "   |    "
+        table+= "\n"
         table += "-" * leap + "\n"
         for i in range(len(self.table)):
             table += f"|   " + self.basic_variables[i] +"   |   "
@@ -51,7 +44,44 @@ class TableSimplex:
             else:
                 self.basic_variables[i] = f"H{i}"
 
-    def solve(self):
+        a = 1
+        for i in range(self.num_variables + self.num_equations):
+            if i == 0:
+                self.variable[i] = "VB"
+            elif i <= self.num_variables and self.num_variables >0:
+                self.variable[i] = f"X{i}"
+            else:
+                self.variable[i] = f"H{a}"
+                a += 1
+
+            self.variable[len((self.variable)) - 1] = f"LD"
+
+
+    def __output_variable__(self,position):
+        values_ld = np.zeros(len(self.table), dtype=int)
+        values_column = np.zeros(len(self.table), dtype= int)        
+        values = np.zeros(len(self.table) - 1,dtype=int)
+
+
+        for i in range(len(self.table)):
+            values_ld[i] = self.table[i].row[-1][1]
+            values_column[i] = self.table[i].row[position][1]
+
+        for i in range(len(values_ld[1:])):
+            if values_column[i + 1] > 0:
+                values[i] = values_ld[i + 1] / values_column[i + 1]
+
+        menor = values[values > 0]
+
+        return  int(np.where(values == values[menor.argmin()])[0] + 1)
+
+        
+
+
+
+
+    def solve_max(self):
         return 0
 
-
+    def solve_min(self):
+        return 0
--- test.py
+++ test.py
@@ -5,7 +5,6 @@ from numpy import array
 table_dict = {
     "num_ecuaciones": 5,
     "num_variables" : 2,
-    "positions" : 0,
     "table": array([
         [[0,-3],[0,-2], [0,0], [0,0], [0,0], [0,0], [0,0]],
         [[0,1], [0,2], [0,1], [0,0], [0,0], [0,0], [0,6]],
@@ -15,12 +14,11 @@ table_dict = {
     ])
 }
 
-table = TableSimplex(table_dict["num_ecuaciones"],table_dict["num_variables"],table_dict["positions"])
+table = TableSimplex(table_dict["num_ecuaciones"],table_dict["num_variables"])
 
 table.initialize_table(table_dict["table"])
-
-a = table.table[0].multiplyrow_bynumber(2)
-print(a)
 print(table)
+
+print(table.__output_variable__(2))
 
 
